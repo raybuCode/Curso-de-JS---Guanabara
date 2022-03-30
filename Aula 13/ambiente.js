98291e90f09c9ed18956ee50a085b6d0765676c2
Raybu <raymydrive@gmail.com>
2022-03-30 14:48:22 -0300

    aula 13 inicio

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,15 @@
+/* Estrutura de repetição com teste logico no INICIO
+var passos = 1
+
+ while(passos <= 10){
+    console.log(`Voce deu o passo numero ${passos}`)
+    passos++
+ }
+ */
+
+//Estrutura de repetição com teste logico no FINAL
+var passos = 1
+do{
+    console.log(`Voce deu o passo numero ${passos}`)
+    passos++
+} while(passos <= 10)
