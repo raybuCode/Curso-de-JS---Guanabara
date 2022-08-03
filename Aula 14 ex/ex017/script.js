79bab3bd227fc3986aa3ba5f86b79d54e3024b14
Raybu <raymydrive@gmail.com>
2022-08-03 19:21:31 -0300

    Ultimo exercicio aula 14
    Foi feitos os dois exercicios da aula 14.. nesse push estou enviando o segundo

--- Aula 14 ex/ex017/script.js
+++ Aula 14 ex/ex017/script.js
@@ -1,1 +1,24 @@
-alert('Ola')
+function tabuada(){
+    let num = document.getElementById("txtn");
+    let tab = document.getElementById("seltab") 
+
+    if (num.value == 0){
+        window.alert('Favor digitar um numero')
+    } else{
+        let n = Number(num.value)
+        let c= 1 
+        tab.innerHTML= ''
+        for (c = 0; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+
+   
+
+
+
+
+}
